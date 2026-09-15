aab7d6fab7e5452b1823499a6e0249561b25d991
miscberry <chris@chrisbennett.ca>
2026-09-15 13:17:17 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,8 @@
+public class Date {
+	public static void main(String[] args) {
+		int day = 15;
+		String month = "September";
+		int year = 2026;
+		System.out.print("The current date is " + month + " " + day + ", " + year + ".");
+	}
+}
