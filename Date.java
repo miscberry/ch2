87f9bf334dcb245cb1a94d6b1481e217e1662138
miscberry <chris@chrisbennett.ca>
2026-09-22 15:45:47 -0700

    Add ch2 exercises

--- Date.java
+++ Date.java
@@ -1,8 +1,12 @@
 public class Date {
-	public static void main(String[] args) {
-		int day = 15;
-		String month = "September";
-		int year = 2026;
-		System.out.print("The current date is " + month + " " + day + ", " + year + ".");
-	}
+
+    public static void main(String[] args) {
+        String day = "Tuesday";
+        int date = 22;
+        String month = "September";
+        int year = 2026;
+
+        System.out.println("American format: " + day + ", " + month + " " + date + ", " + year);
+        System.out.println("European format: " + day + " " + date + " " + month + " " + year);
+    }
 }
--- Time.java
+++ Time.java
@@ -1,9 +1,26 @@
 public class Time {
-	public static void main(String[] args) {
-		int hours = 11;
-		int minutes = 59;
-		int seconds = ((hours * 60) + minutes) * 60;
-		System.out.print("It has been " + seconds + " seconds since midnight.");
 
-	}
+    public static void main(String[] args) {
+        int hour = 14;
+        int minute = 30;
+        int second = 45;
+
+        int secondsSinceMidnight = hour * 3600 + minute * 60 + second;
+        System.out.println("Seconds since midnight: " + secondsSinceMidnight);
+
+        int secondsInDay = 24 * 3600;
+        int secondsRemaining = secondsInDay - secondsSinceMidnight;
+        System.out.println("Seconds remaining in the day: " + secondsRemaining);
+
+        double percentPassed = secondsSinceMidnight * 100.0 / secondsInDay;
+        System.out.println("Percent of the day that has passed: " + percentPassed);
+
+        int endHour = 15;
+        int endMinute = 12;
+        int endSecond = 5;
+
+        int endSeconds = endHour * 3600 + endMinute * 60 + endSecond;
+        int elapsed = endSeconds - secondsSinceMidnight;
+        System.out.println("Seconds spent on this exercise: " + elapsed);
+    }
 }
